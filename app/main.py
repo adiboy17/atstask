3ee28787c4d69e533a504384f10a1c04d041ed03
Govind Singh <61302637+adiboy17@users.noreply.github.com>
2021-10-22 00:18:20 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,8 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    output = sp.getoutput("echo $ATC_USERNAME" + "echo $ATC_PASSWORD")
+    output = sp.getoutput('echo $ATC_USERNAME' \n 'echo $ATC_PASSWORD')
     print(output)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
+    
